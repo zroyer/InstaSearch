7e09711ce4cd9b91373ffa3b6098a201b592bcd1
Zach Royer <zroyer@gmail.com>
2015-04-06 15:40:53 -0500

    submit working

--- client/app.js
+++ client/app.js
@@ -1,9 +1,18 @@
-angular.module('app', []);
+angular.module('app', [])
 
 .controller('searchCtrl', ['$scope', '$http', function($scope, $http) {
-	$scope.tempData = {message: '', results: false};  //submitted?
+	$scope.tempData = {message: '', submitted: false, results: false};  //submitted?
 	$scope.imageData = {data: []};
 
+	$scope.submit = function() {
+		if($scope.mainForm.$valid) {
+			$scope.tempData.query = $scope.tempData.tag.trim();
+			$scope.tempData.tag = "";
+			$scope.message = "Searching Instagram for #" + $scope.tempData.query 
+			$scope.tempData.submitted = true;
+			$scope.getImages($scope.tempData.query);
+		};
+	};
 
-	
+
 }]);
