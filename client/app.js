560426d2623a72430ac8fab6c692c47252487aa0
Zach Royer <zroyer@gmail.com>
2015-05-19 21:03:17 -0500

    more spacing

--- client/app.js
+++ client/app.js
@@ -11,37 +11,35 @@ angular.module('app', [])
       $scope.message = "Searching Instagram for #" + $scope.tempData.query 
       $scope.tempData.submitted = true;
       $scope.getImages($scope.tempData.query);
-		};
-	};
+    };
+  };
 
-	$scope.getImages = function(query) {
-  	var id = "9e51654d8dd64c22aa4933a10dd51194"
+  $scope.getImages = function(query) {
+    var id = "9e51654d8dd64c22aa4933a10dd51194"
     var url = "https://api.instagram.com/v1/tags/" + query + "/media/recent"
     var request = {
       callback: 'JSON_CALLBACK',
       client_id: id
     };
-    
-    $http({
-    	method: 'JSONP',
-      url: url,
-      params: request
-    }).success(function(results){
-    	if (results.meta.code == 200){
-    		if (results.data.length){
-    			$scope.gotResults = true
-    			$scope.images = results.data
-    			$scope.message = 'Now displaying the ' + results.data.length + ' most recent images tagged with #' + $scope.tempData.query 
-    		} 
-    		else{
-    			$scope.message = 'The search for #' + $scope.tempData.query + ' produced no results'
-    		}
-    	}
-    }).error(function(){
-        alert('The search for #' + query + ' was unsuccessful. Please make sure the query string has no spaces.');
-        $scope.message = "";
-    })
+
+  $http({
+    method: 'JSONP',
+    url: url,
+    params: request
+  }).success(function(results){
+    if (results.meta.code == 200){
+      if (results.data.length){
+        $scope.gotResults = true
+        $scope.images = results.data
+        $scope.message = 'Now displaying the ' + results.data.length + ' most recent images tagged with #' + $scope.tempData.query 
+      } 
+      else{
+        $scope.message = 'The search for #' + $scope.tempData.query + ' produced no results'
+      }
+    }
+  }).error(function(){
+    alert('The search for #' + query + ' was unsuccessful. Please make sure the query string has no spaces.');
+    $scope.message = "";
+  })
 };
-
-
 }]);
